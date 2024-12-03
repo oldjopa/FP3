b2eced427b46c3745900f87740c7cc4206615467
Leonid <50008736+lolkek22854@users.noreply.github.com>
2024-12-03 21:25:13 +0300

    quickfix linear

--- lab3/linear_interpolate.fs
+++ lab3/linear_interpolate.fs
@@ -1,13 +1,15 @@
 module linear_interpolate
 
 let linearInterpolate (x1, y1) (x2, y2) (samplingRate: float) =
-    seq {
-        let step = samplingRate
-        let mutable x = x1
-
-        while x <= x2 do
+    let rec loop x =
+        if x <= x2 then
             let t = (x - x1) / (x2 - x1)
             let y = y1 + t * (y2 - y1)
-            yield (x, y)
-            x <- x + step
-    }
+            seq {
+                yield (x, y)
+                yield! loop (x + samplingRate)
+            }
+        else
+            Seq.empty
+
+    loop x1
